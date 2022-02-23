c755e0308b8326b89307107dd9cd334ba5de4734
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-23 11:43:10 +0900

    Footer Element Added

---
+++ build/precache-manifest.8718d48a4d015eb0eda9f7c5b4a2fe44.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061cbe7bf4c3b30b34039dc34d4d7a53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d78519df44ec2c64bd4e",
+    "url": "/static/js/2.ec2d356b.chunk.js"
+  },
+  {
+    "revision": "d96d0f4a6078f568292f",
+    "url": "/static/js/main.9cab9830.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
