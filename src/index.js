5cf46b9095b0eaa74c4b1152bede31116560c43d
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-23 11:06:10 +0900

    React App and App.jsx component added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-//1. Create a new React app.
-//2. Create a App.jsx component.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />,
+  document.getElementById("root")
+  );
+
+
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
 //4. Create a Footer.jsx component that renders a <footer> element
